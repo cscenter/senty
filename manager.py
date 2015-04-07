d6d6567fab770b71659d19b125b9a9eb58fdc12d
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-08 00:27:32 +0300

    adding manager, not working version yet

--- manager.py
+++ manager.py
@@ -1,11 +1,13 @@
 # encoding: UTF-8
 
 training_data = 'data/training_data'
-extractor_data = 'data/extractor'
+extractor_data = 'data/extractor_data'
 
 def main():
-    
-    print 'privet'
+    pass
+    # обращаемся к экстрактору, он создаёт данные в папке training_data
+    # обращаемся к ml, оно работает с данными из training_data
+    # тестим качество на данных testing_data
 
 if __name__ == '__main__':
     main()
