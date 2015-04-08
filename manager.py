022d675f0ab8bc281a3717cae179ab151a308f50
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-08 12:42:55 +0300

    added work with extractor

--- manager.py
+++ manager.py
@@ -1,12 +1,21 @@
 # encoding: UTF-8
 
-training_data = 'data/training_data'
-extractor_data = 'data/extractor_data'
+import sys
+sys.path.append('extractors/')
+import extractor1
+
+training_data = '../data/training_data'
+extractor_data = '../data/extractor_data'
 
 def main():
-    pass
     # обращаемся к экстрактору, он создаёт данные в папке training_data
+    extractor = extractor1.extractor(extractor_data, training_data)
+    if extractor.extract() == False:    
+        raise Exception('error in extractor')
+    
     # обращаемся к ml, оно работает с данными из training_data
+        
+    
     # тестим качество на данных testing_data
 
 if __name__ == '__main__':
