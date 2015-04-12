1e4e544bce1db776086db788fbb83008129ac4e1
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-12 22:56:59 +0300

    added testing svm

--- manager.py
+++ manager.py
@@ -5,6 +5,7 @@ sys.path.append('extractors/')
 import extractor1
 sys.path.append('ml/')
 import naive_bayes_gaussian
+import svm
 
 testing_data = 'data/testing_data/'
 training_data = 'data/training_data/'
@@ -19,20 +20,28 @@ def main():
     
     # обращаемся к ml, оно работает с данными из training_data
     nbg = NaiveBayesGaussian(training_data)
-    nbg.fit()    
+    nbg.fit()
+    my_svm = svm.SVM(training_data)
+    my_svm.fit()    
     
     # тестим качество на данных testing_data
     input_files = filter(lambda x: not x.endswith('~'), os.listdir(testing_data))
-    ok = 0
+    testing_files_count = len(os.listdir(testing_data))         
+    nbg_true = 0
+    svm_true = 0
     for f in input_files:
         bash = json.load(open(os.path.join(testing_data, f)), 'utf-8')
         real_class = 1
         if bash['positive'] == 'No':
             real_class = -1
-        y = nbg.predict(testing_data + f)
-        if y == real_class:
-            ok += 1
-    print float(ok) / len(os.listdir(testing_data))         
+        nbg_class = nbg.predict(testing_data + f)
+        svm_class = my_svm.predict(testing_data + f)
+        if nbg_class == real_class:
+            nbg_true += 1
+        if svm_class == real_class:
+            svm_true += 1
+    print float(nbg_true) / testing_files_count
+    print float(svm_true) / testing_files_count
 
 if __name__ == '__main__':
     main()
