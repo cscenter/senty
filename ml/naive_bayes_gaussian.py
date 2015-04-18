295d558bd577bfbcb52c8b3fddd86f0b5ac6ebfb
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-18 13:20:14 +0300

    remove excess code

--- ml/naive_bayes_gaussian.py
+++ ml/naive_bayes_gaussian.py
@@ -1,15 +1,8 @@
 # encoding: UTF-8
 
-import os
-import json
 import machine_learning
 
-from collections import namedtuple
-
 from sklearn.naive_bayes import GaussianNB
-
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 class NaiveBayesGaussian(machine_learning.MachineLearning):        
     def __init__(self, training_data_path):
@@ -23,19 +16,3 @@ class NaiveBayesGaussian(machine_learning.MachineLearning):
     def predict(self, json_file_path):
         bash_data = machine_learning.MachineLearning.predict_data(self, json_file_path)
         return self.gnb.predict(bash_data)  
-
-def main():
- #   DEBUG_training_data = '../data/training_data/' 
- #   DEBUG_test_file = '../data/testing_data/23_tf-idf'
- #   nb = NaiveBayesGaussian(DEBUG_training_data)
- #   nb.fit()
-  #  y = nb.predict(DEBUG_test_file)
-    #if y == -1:
-    #    y = 'No'
-    #else:
-    #    y = 'Yes'
-  #  print y    
-    pass
-
-if __name__ == '__main__':
-    main()
--- ml/svm.py
+++ ml/svm.py
@@ -1,14 +1,8 @@
 # encoding: UTF-8
 
-import os
-import json
 import machine_learning
 
-from collections import namedtuple
 from sklearn.svm import LinearSVC
-
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 class SVM(machine_learning.MachineLearning):        
     def __init__(self, training_data_path):
@@ -22,19 +16,3 @@ class SVM(machine_learning.MachineLearning):
     def predict(self, json_file_path):
         bash_data = machine_learning.MachineLearning.predict_data(self, json_file_path)
         return self.svm.predict(bash_data) 
-
-def main():
- #   DEBUG_training_data = '../data/training_data/' 
- #   DEBUG_test_file = '../data/testing_data/23_tf-idf'
- #   nb = NaiveBayesGaussian(DEBUG_training_data)
- #   nb.fit()
-  #  y = nb.predict(DEBUG_test_file)
-    #if y == -1:
-    #    y = 'No'
-    #else:
-    #    y = 'Yes'
-  #  print y    
-    pass
-
-if __name__ == '__main__':
-    main()
