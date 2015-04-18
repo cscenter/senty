bbfe0c15d83ba4379e6735196cf4dfccc6836594
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-18 13:12:05 +0300

    Changed all structure of machine learning: MachineLearning not is abstract class now, but others ml inhabited it still

--- ml/machine_learning.py
+++ ml/machine_learning.py
@@ -1,14 +1,58 @@
 # encoding: UTF-8
 
-from abc import ABCMeta, abstractmethod, abstractproperty
+import os
+import json
 
 class MachineLearning():
-    __metaclass__ = ABCMeta
-    
-    @abstractmethod
-    def fit():
-        pass
+    def __init__(self, training_data_path):
+        self.term_num = {}
+        self.terms_count = 0
+        self.training_data_path = training_data_path
+
+    def fit_data(self):
+        input_files = filter(lambda x: not x.endswith('~'), os.listdir(self.training_data_path))
+        all_words = set()
+        for f in input_files:
+            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
+            for word in bash['terms']:
+                all_words.add(word)
         
-    @abstractmethod
-    def predict():  
-        pass    
+        iter = 0
+        self.terms_count = len(all_words)
+        for word in all_words:
+            self.term_num[word] = iter
+            iter += 1
+        
+        train_data = []
+        target = []
+        for f in input_files:
+            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
+            senty = 1
+            if bash['positive'] == 'No':
+                senty = -1
+            target.append(senty)    
+            bash_data = []
+            for iter in range(0, self.terms_count):
+                bash_data.append(0)
+            bash_dict = dict(bash['terms'])
+            for word in bash['terms']:
+                tf = bash_dict[word][u'tf']
+                idf = bash_dict[word][u'idf']
+                bash_data[self.term_num[word]] = tf * idf
+            train_data.append(bash_data)    
+        return (train_data, target)        
+    
+    def predict_data(self, json_file_path):  
+        bash = json.load(open(json_file_path), 'utf-8')
+        bash_data = []
+        for iter in range(0, self.terms_count):
+            bash_data.append(0)
+        bash_dict = dict(bash['terms'])
+        for word in bash['terms']:
+            if word not in self.term_num:
+                continue
+            tf = bash_dict[word][u'tf']
+            idf = bash_dict[word][u'idf']
+            bash_data[self.term_num[word]] = tf * idf
+        return bash_data    
+        
--- ml/naive_bayes_gaussian.py
+++ ml/naive_bayes_gaussian.py
@@ -13,60 +13,16 @@ def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 class NaiveBayesGaussian(machine_learning.MachineLearning):        
     def __init__(self, training_data_path):
+        machine_learning.MachineLearning.__init__(self, training_data_path)
         self.gnb = GaussianNB()
-        self.term_num = {}
-        self.terms_count = 0
-        self.training_data_path = training_data_path
-        
+
     def fit(self):
-        input_files = filter(lambda x: not x.endswith('~'), os.listdir(self.training_data_path))
-        all_words = set()
-        for f in input_files:
-            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
-            for word in bash['terms']:
-                all_words.add(word)
-        
-        iter = 0
-        self.terms_count = len(all_words)
-        for word in all_words:
-            self.term_num[word] = iter
-            iter += 1
-        
-        train_data = []
-        target = []
-        for f in input_files:
-            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
-            senty = 1
-            if bash['positive'] == 'No':
-                senty = -1
-            target.append(senty)    
-            bash_data = []
-            for iter in range(0, self.terms_count):
-                bash_data.append(0)
-            bash_dict = dict(bash['terms'])
-            for word in bash['terms']:
-                tf = bash_dict[word][u'tf']
-                idf = bash_dict[word][u'idf']
-                bash_data[self.term_num[word]] = tf * idf
-            train_data.append(bash_data)    
+        train_data, target = machine_learning.MachineLearning.fit_data(self)
         self.gnb.fit(train_data, target)        
-        #сначала получить список всех различных файлов в сет
-        #во втором пробеге уже составлять вектора    
     
     def predict(self, json_file_path):
-        bash = json.load(open(json_file_path), 'utf-8')
-        bash_data = []
-        for iter in range(0, self.terms_count):
-            bash_data.append(0)
-        bash_dict = dict(bash['terms'])
-        for word in bash['terms']:
-            if word not in self.term_num:
-                continue
-            tf = bash_dict[word][u'tf']
-            idf = bash_dict[word][u'idf']
-            bash_data[self.term_num[word]] = tf * idf
+        bash_data = machine_learning.MachineLearning.predict_data(self, json_file_path)
         return self.gnb.predict(bash_data)  
-
 
 def main():
  #   DEBUG_training_data = '../data/training_data/' 
