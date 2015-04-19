805541337c88b6a83354fa552aed90443a5b0c4d
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-19 14:08:10 +0300

    adding universal function get_quality that can work with all types of ml, return only accurancy now

--- manager.py
+++ manager.py
@@ -16,11 +16,26 @@ training_data = 'data/training_data/'
 extractor_training_data = 'data/extractor_data/training_data/'
 extractor_testing_data = 'data/extractor_data/testing_data/'
 
-NEW_EXTRACTOR = True
-#NEW_EXTRACTOR = False
+# return accuracy, TP, TN, FP, FN
+def get_quality(ml):
+    input_files = filter(lambda x: not x.endswith('~'), os.listdir(testing_data))
+    testing_files_count = len(os.listdir(testing_data))         
+    true_count = 0
+    for f in input_files:
+        bash = json.load(open(os.path.join(testing_data, f)), 'utf-8')
+        real_class = 1
+        if bash['positive'] == 'No':
+            real_class = -1
+        ml_class = ml.predict(testing_data + f)
+        if ml_class == real_class:
+            true_count += 1
+    return float(true_count) / testing_files_count
+
+#NEW_EXTRACTOR = True
+NEW_EXTRACTOR = False
 
 def main():
-    # обращаемся к экстрактору, он создаёт данные в папке training_data
+    # обращаемся к экстрактору, он создаёт данные в папках training_data и testing_data
     if NEW_EXTRACTOR:
         # очистить папки training и testing data        
         if os.path.exists(training_data):
@@ -46,30 +61,17 @@ def main():
             raise Exception('error in extractor for testing')
         
     # обращаемся к ml, оно работает с данными из training_data
-    
+ 
+    # тестим качество на данных testing_data   
+ 
+    # 1. NBG 
     nbg = naive_bayes_gaussian.NaiveBayesGaussian(training_data)
-    nbg.fit()
+    nbg.fit()   
+    print get_quality(nbg)
+    # 2. LinearSVC    
     my_svm = svm.SVM(training_data)
-    my_svm.fit()    
+    my_svm.fit()
+    print get_quality(my_svm)    
     
-    # тестим качество на данных testing_data
-    input_files = filter(lambda x: not x.endswith('~'), os.listdir(testing_data))
-    testing_files_count = len(os.listdir(testing_data))         
-    nbg_true = 0
-    svm_true = 0
-    for f in input_files:
-        bash = json.load(open(os.path.join(testing_data, f)), 'utf-8')
-        real_class = 1
-        if bash['positive'] == 'No':
-            real_class = -1
-        nbg_class = nbg.predict(testing_data + f)
-        svm_class = my_svm.predict(testing_data + f)
-        if nbg_class == real_class:
-            nbg_true += 1
-        if svm_class == real_class:
-            svm_true += 1
-    print float(nbg_true) / testing_files_count
-    print float(svm_true) / testing_files_count
-
 if __name__ == '__main__':
     main()
