4360f7b3f116940cb5f679d0f72f001e6b24748e
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-19 14:41:24 +0300

    adding fit_data_1_0and predict_data_1_0

--- ml/machine_learning.py
+++ ml/machine_learning.py
@@ -55,4 +55,45 @@ class MachineLearning():
             idf = bash_dict[word][u'idf']
             bash_data[self.term_num[word]] = tf * idf
         return bash_data    
+
+    def fit_data_1_0(self):
+        input_files = filter(lambda x: not x.endswith('~'), os.listdir(self.training_data_path))
+        all_words = set()
+        for f in input_files:
+            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
+            for word in bash['terms']:
+                all_words.add(word)
+        
+        iter = 0
+        self.terms_count = len(all_words)
+        for word in all_words:
+            self.term_num[word] = iter
+            iter += 1
+        
+        train_data = []
+        target = []
+        for f in input_files:
+            bash = json.load(open(os.path.join(self.training_data_path, f)), 'utf-8')
+            senty = 1
+            if bash['positive'] == 'No':
+                senty = -1
+            target.append(senty)    
+            bash_data = []
+            for iter in range(0, self.terms_count):
+                bash_data.append(0)
+            for word in bash['terms']:
+                bash_data[self.term_num[word]] = 1
+            train_data.append(bash_data)    
+        return (train_data, target)        
+    
+    def predict_data_1_0(self, json_file_path):  
+        bash = json.load(open(json_file_path), 'utf-8')
+        bash_data = []
+        for iter in range(0, self.terms_count):
+            bash_data.append(0)
+        for word in bash['terms']:
+            if word not in self.term_num:
+                continue
+            bash_data[self.term_num[word]] = 1
+        return bash_data   
         
