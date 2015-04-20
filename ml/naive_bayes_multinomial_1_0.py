441c36018f968dfd192ecf68a58776f48fc266d7
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-04-20 20:47:43 +0300

    add CV for naive_bayes_multinomial(not usefull yet)

--- ml/naive_bayes_multinomial_1_0.py
+++ ml/naive_bayes_multinomial_1_0.py
@@ -7,12 +7,42 @@ from sklearn.naive_bayes import MultinomialNB
 class NaiveBayesMultinomial(machine_learning.MachineLearning):        
     def __init__(self, training_data_path):
         machine_learning.MachineLearning.__init__(self, training_data_path)
-        self.gnb = MultinomialNB(alpha = 1.0)
+        self.data_for_fit = machine_learning.MachineLearning.fit_data_1_0(self)
+        
+     #   best_alpha = 3.1321 # установлена по сиви, запустить сиви сново при новом обучающем множестве
+        #best_alpha = self.get_best_alpha_cv()
+        best_alpha = 1.0
+        self.gnb = MultinomialNB(alpha = best_alpha)
 
     def fit(self):
-        train_data, target = machine_learning.MachineLearning.fit_data_1_0(self)
-        self.gnb.fit(train_data, target)        
+        self.gnb.fit(self.data_for_fit[0], self.data_for_fit[1])        
     
     def predict(self, json_file_path):
         bash_data = machine_learning.MachineLearning.predict_data_1_0(self, json_file_path)
-        return self.gnb.predict(bash_data)  
+        return self.gnb.predict(bash_data)
+        
+    def get_best_alpha_cv(self):
+        best_alpha = 1.0
+        best_score = 0
+        from sklearn import cross_validation
+        test_alpha = 0.01
+        step = 0.01
+        n = 5 # num of parts
+        while test_alpha < 10:
+            self.gnb = MultinomialNB(test_alpha)
+            scores = cross_validation.cross_val_score(self.gnb, self.data_for_fit[0], 
+                                                      self.data_for_fit[1], cv = n)
+            print str(test_alpha) + ': ' + str(scores.mean())            
+            if scores.mean() > best_score:
+                best_score = scores.mean()
+                best_alpha = test_alpha
+            test_alpha += step
+        print 'best_alpha: ' + str(best_alpha) + ', with score: ' + str(best_score)    
+        return best_alpha
+
+#DEBUG = True
+DEBUG = False
+
+if DEBUG:
+    nbm = NaiveBayesMultinomial("../data/training_data/")        
+    nbm.fit()
