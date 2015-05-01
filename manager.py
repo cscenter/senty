734ed3867f9cc7e5a99e8ed3d8fdcdee049ede7f
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-05-02 01:33:56 +0300

    added working version of function getDiffBetweenExtractors

--- manager.py
+++ manager.py
@@ -6,6 +6,7 @@ import json
 import sys
 sys.path.append('extractors/')
 import standard_extractor
+import standard_extractor_with_not
 #import n_gram_extractor
 #import more_than_n_gram_extractor
 sys.path.append('ml/')
@@ -44,38 +45,49 @@ def getFalsesInExtractor(ml, extractor):
     return log_with_falses
 
 def getDiffBetweenExtractors(ml, extractor1, extractor2):
-    pass
+    #log1 = getFalsesInExtractor(ml, extractor1)
+    #log2 = getFalsesInExtractor(ml, extractor2)
+    log1 = getMlLogInExtractor(ml, extractor1)
+    log2 = getMlLogInExtractor(ml, extractor2)
+    diff_log1_log2 = []
+    for l in log1:
+        ok = False
+        res = ""
+        for l2 in log2:
+            if l[0] == l2[0] and l[1] != l2[1]:
+                ok = True
+                res = str(l[0]) + '; extr1): ' + str(l[1]) + '; extr2): ' + str(l2[1]) + '; really: ' + str(l[2]) 
+                break
+        if ok == True:
+            diff_log1_log2.append(res)
+    return diff_log1_log2        
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 def main():
 #    extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
+#    extractor2 = standard_extractor_with_not.standard_extractor(marked_data, extractor_data)
 #    nbg_count = naive_bayes_gaussian_count.NaiveBayesGaussian(extractor_data)
 #    getFalsesInExtractor(nbg_count, extractor1)    
+#    diff = getDiffBetweenExtractors(nbg_count, extractor1, extractor2)    
 
-#    extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
-#    mnb_count = naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data)
-#    getFalsesInExtractor(mnb_count, extractor1)    
-    
     extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
-    my_svm_1_0 = svm_1_0.SVM(extractor_data)
-    getFalsesInExtractor(my_svm_1_0, extractor1)    
-
+    mnb_count = naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data) 
+    extractor2 = standard_extractor_with_not.standard_extractor(marked_data, extractor_data)    
+    diff = getDiffBetweenExtractors(mnb_count, extractor1, extractor2)    
+    
+#    extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
+#    extractor2 = standard_extractor_with_not.standard_extractor(marked_data, extractor_data)
+#    my_svm_1_0 = svm_1_0.SVM(extractor_data)
+  #  diff = getDiffBetweenExtractors(my_svm_1_0, extractor1, extractor2)    
+    f = open('diff.txt', 'w')
+    for d in diff:
+        f.write(d + '\n')
+    f.close()
+    print 'Diffrence has been writen into diff.txt'
 #    extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
 #    my_svm_tf_idf = svm_tf_idf.SVM(extractor_data)
 #    getFalsesInExtractor(my_svm_tf_idf, extractor1)    
     
     '''
-    # 3. LinearSVC 1 0    
-    my_svm_1_0 = svm_1_0.SVM(marked_data)
-    my_svm_1_0.fit()
-    print 'SVC with 1 0'
-    get_quality(my_svm_1_0)        
-    
-    # 4. LinearSVC tf idf    
-    my_svm_tf_idf = svm_tf_idf.SVM(marked_data)
-    my_svm_tf_idf.fit()
-    print 'SVC with tf idf'
-    get_quality(my_svm_tf_idf)    
-    
     # 5. Logistic Regression 1 0
     lg_with_1_0 = logistic_regression_1_0.LG(marked_data)
     lg_with_1_0.fit()
