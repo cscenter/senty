d018454b8fde34679bcf233fa68c6887050b3b3b
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-05-01 22:58:23 +0300

    changed work with ml: working with testing and training data as if its one object

--- manager.py
+++ manager.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('extractors/')
 import standard_extractor
 #import n_gram_extractor
-#import more_than_n_gram_extractor
+import more_than_n_gram_extractor
 sys.path.append('ml/')
 import naive_bayes_gaussian_count
 import naive_bayes_multinomial_count
@@ -15,11 +15,10 @@ import svm_1_0
 import svm_tf_idf
 import logistic_regression_1_0
 
-testing_data = 'data/testing_data/'
-training_data = 'data/training_data/'
-extractor_training_data = 'data/extractor_data/training_data/'
-extractor_testing_data = 'data/extractor_data/testing_data/'
+marked_data = 'data/marked_data/'
+extractor_data = 'data/extractor_data/'
 
+'''
 # print accuracy, TP, TN, FP, FN
 def get_quality(ml):
     input_files = filter(lambda x: not x.endswith('~'), os.listdir(testing_data))
@@ -49,71 +48,71 @@ def get_quality(ml):
     print 'Accuracy: ' + str(float(true_count) / testing_files_count)
     print 'All test count: ' + str(testing_files_count) + '; TP: ' + str(TP) + '; TN: ' + str(TN) + '; FP: ' + str(FP) + '; FN: ' + str(FN)  
     print ''    
+'''    
+
+def getMlResults(ml):
+    return ml.predict(0.05)
     
-NEW_EXTRACTOR = True
-#NEW_EXTRACTOR = False
+def getMlLogInExtractor(ml, extractor):    
+    if os.path.exists(extractor_data):
+        files = filter(lambda x: not x.endswith('~'), os.listdir(extractor_data))
+        for f in files:
+            os.remove(extractor_data + f)
+        os.rmdir(extractor_data)
+    os.mkdir(extractor_data)    
+    if extractor.extract() == False:     
+        raise Exception('error in extractor')    
+    log = getMlResults(ml)
+    return log
+    
+def getFalsesInExtractor(ml, extractor):
+    log = getMlLogInExtractor(ml, extractor)
+    log_with_falses = []
+    for l in log:
+        if l[1] != l[2]:
+            log_with_falses.append(l)
+    print len(log_with_falses)        
+    return log_with_falses
+
+def getDiffBetweenExtractors(ml, extractor1, extractor2):
+    pass
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 def main():
-    # обращаемся к экстрактору, он создаёт данные в папках training_data и testing_data
-    if NEW_EXTRACTOR:
-        # очистить папки training и testing data        
-        if os.path.exists(training_data):
-            files = filter(lambda x: not x.endswith('~'), os.listdir(training_data))
-            for f in files:
-                os.remove(training_data + f)
-            os.rmdir(training_data)
-        os.mkdir(training_data)    
-        if os.path.exists(testing_data):    
-            files = filter(lambda x: not x.endswith('~'), os.listdir(testing_data))
-            for f in files:
-                os.remove(testing_data + f)
-            os.rmdir(testing_data)        
-        os.mkdir(testing_data)
-        
-
-        extractor_for_training = standard_extractor.standard_extractor(extractor_training_data, training_data)
-        #extractor_for_training = more_than_n_gram_extractor.n_gramm_extractor(extractor_training_data, training_data, 2)
-        if extractor_for_training.extract() == False:    
-            raise Exception('error in extractor for training')
-        
-        extractor_for_testing = standard_extractor.standard_extractor(extractor_testing_data, testing_data)
-      #  extractor_for_testing = more_than_n_gram_extractor.n_gramm_extractor(extractor_testing_data, testing_data, 2)
-        if extractor_for_testing.extract() == False:    
-            raise Exception('error in extractor for testing')
-        
-    # обращаемся к ml, оно работает с данными из training_data
- 
-    # тестим качество на данных testing_data   
-    
+    extractor1 = standard_extractor.standard_extractor(marked_data, extractor_data)
+  #  nbg_count = naive_bayes_gaussian_count.NaiveBayesGaussian(marked_data)
+    mnb_count = naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data)
+    getFalsesInExtractor(mnb_count, extractor1)    
+    '''
     # 1. NBG count
-    nbg_count = naive_bayes_gaussian_count.NaiveBayesGaussian(training_data)
+    nbg_count = naive_bayes_gaussian_count.NaiveBayesGaussian(marked_data)
     nbg_count.fit()   
     print 'Naive Bayes Gaussian with count'
     get_quality(nbg_count)
 
     # 2. MBG count
-    mbg_count = naive_bayes_multinomial_count.NaiveBayesMultinomial(training_data)
+    mbg_count = naive_bayes_multinomial_count.NaiveBayesMultinomial(marked_data)
     mbg_count.fit()   
     print 'Naive Bayes Multinomial with count'
     get_quality(mbg_count)
 
     # 3. LinearSVC 1 0    
-    my_svm_1_0 = svm_1_0.SVM(training_data)
+    my_svm_1_0 = svm_1_0.SVM(marked_data)
     my_svm_1_0.fit()
     print 'SVC with 1 0'
     get_quality(my_svm_1_0)        
     
     # 4. LinearSVC tf idf    
-    my_svm_tf_idf = svm_tf_idf.SVM(training_data)
+    my_svm_tf_idf = svm_tf_idf.SVM(marked_data)
     my_svm_tf_idf.fit()
     print 'SVC with tf idf'
     get_quality(my_svm_tf_idf)    
     
     # 5. Logistic Regression 1 0
-    lg_with_1_0 = logistic_regression_1_0.LG(training_data)
+    lg_with_1_0 = logistic_regression_1_0.LG(marked_data)
     lg_with_1_0.fit()
     print 'Logistic Regression with 1 0'
     get_quality(lg_with_1_0)    
+    '''
     
 if __name__ == '__main__':
     main()
