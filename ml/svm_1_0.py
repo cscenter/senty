2a554b07c46bf32059265efc464003e5d3800ddb
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-05-03 22:17:04 +0300

    add beta version of txq CV

--- ml/svm_1_0.py
+++ ml/svm_1_0.py
@@ -8,7 +8,7 @@ class SVM(machine_learning.MachineLearning):
     def __init__(self, training_data_path):
         machine_learning.MachineLearning.__init__(self, training_data_path)
 
-    def predict(self, block_size_in_ratio):
+    def predict(self, t):
         self.data_for_fit = machine_learning.MachineLearning.fit_data_1_0(self)
         self.input_files = machine_learning.MachineLearning.getInputFiles(self)
         
@@ -50,4 +50,27 @@ class SVM(machine_learning.MachineLearning):
         
         print 'SVC with 1 0: ' + str(round(100. * float(totalTrue) / N, 2)) + '%'
         return result  
-        
+      
+    def qtCV(self, t):        
+        self.data_for_fit = machine_learning.MachineLearning.fit_data_1_0(self)
+        from sklearn import cross_validation
+        all_scores = 0
+        for test in range(0, t):
+            print 'test number ' + str(test + 1)
+            self.svm = LinearSVC(C = 2)
+            import random
+            cv = cross_validation.ShuffleSplit(len(self.data_for_fit[0]), n_iter = 10, test_size = 0.1, random_state = random.randint(0, 255))
+            print cv
+            scores = cross_validation.cross_val_score(self.svm, self.data_for_fit[0], self.data_for_fit[1], cv=cv)            
+            
+            print 'mean: ' + str(scores.mean())
+            all_scores += scores.mean()
+            print ''
+        print all_scores / float(t)   
+      
+#DEBUG = True
+DEBUG = False
+
+if DEBUG:
+    svm_1_0 = SVM("../data/extractor_data/")        
+    svm_1_0.predict(5)
