7c2ba010240e3114f16c2b4d284c8ed4e4a8d765
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-05-10 16:57:19 +0300

    add using for a few features

--- manager.py
+++ manager.py
@@ -94,12 +94,15 @@ def main():
     os.mkdir(middle_data)    
     delete_folder_with_files(extractor_data)
     os.mkdir(extractor_data)    
+
+    # обязательная насадка без удаления повторяющихся букв - прогонка через mystem + убираем знаки препинания
+    #print 'Прогоняем через mystem и убираем знаки препинания...'
+    #features.mystem_using(marked_data, middle_data)    
     
-    # обязательная насадка - прогонка через mystem + убираем знаки препинания
-    print 'Прогоняем через mystem и убираем знаки препинания...'
-    features.mystem_using(marked_data, middle_data)
-    # окончание обязательной насадки 
-    
+    # обязательная насадка с удалением повторяющихся букв - прогонка через mystem + убираем знаки препинания    
+    print 'Прогоняем через mystem (удаляем подряд идущие повторяющиеся буквы) и убираем знаки препинания...'
+    features.mystem_using_with_considering_of_multiple_letters(marked_data, middle_data)    
+        
     # удаление предлогов
     #print 'Убираем предлоги...'
     #features.without_prepositions(middle_data)    
@@ -112,19 +115,31 @@ def main():
     #print 'Убираем местоимения...'
     #features.without_pronouns(middle_data)         
     
+    # убирает повторяющиеся буквы, фича, не насадка
+    print 'Удаляем подряд идущие повторяющиеся буквы...' 
+    features.considering_of_multiple_letters(middle_data)
+    
+    # частица не
+    print 'Присоединяем частицу не к словам не состояниям...'
+    features.with_not(middle_data)    
+    
+    # убираем термы на английском языке
+    #print 'Убираем термы на английском языке...'
+    #features.without_foreign_words(middle_data)    
+    
     # n-граммы
-    #print 'N граммы...'
-    #features.more_than_n_gram_feature(2, middle_data)
+   # print 'N граммы...'
+   # features.more_than_n_gram_feature(2, middle_data)
     
     print 'Экстрактор работает...'    
     my_extractor = extractor.extractor(middle_data, extractor_data)
     if my_extractor.extract() == False:
         raise Exception('Error in extractor!')
-
+        
     print 'Запускаем машинное обучение...' 
     #executeMlAndPrintAccurancy(naive_bayes_gaussian_count.NaiveBayesGaussian(extractor_data))
-    #executeMlAndPrintAccurancy(naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data))
-    executeMlAndPrintAccurancy(svm_1_0.SVM(extractor_data))
+    executeMlAndPrintAccurancy(naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data))
+    #executeMlAndPrintAccurancy(svm_1_0.SVM(extractor_data))
     #executeMlAndPrintAccurancy(svm_tf_idf.SVM(extractor_data))
     #executeMlAndPrintAccurancy(logistic_regression_count.LG(extractor_data))
     
