ce1d5848ef34453d81626a2a732305b7f556dea1
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-05-19 23:30:32 +0300

    some fix, no changes

--- manager.py
+++ manager.py
@@ -76,16 +76,20 @@ def main():
     os.mkdir(extractor_data)     
 
     # до насадки разбираемся с смайликами
-    #print 'Ищем и преобразовываем смайлики...' 
-    #features.emoticon_detection(marked_data, middle_data)
+    print 'Ищем и преобразовываем смайлики...' 
+    features.emoticon_detection(marked_data, middle_data)
     
     # обязательная насадка без удаления повторяющихся букв - прогонка через mystem + убираем знаки препинания
     #print 'Прогоняем через mystem и убираем знаки препинания...'
-    #features.mystem_using(middle_data, middle_data)        
+    #features.mystem_using(marked_data, middle_data)        
     
     # обязательная насадка с удалением повторяющихся букв - прогонка через mystem + убираем знаки препинания    
     print 'Прогоняем через mystem (удаляем подряд идущие повторяющиеся буквы) и убираем знаки препинания...'
-    features.mystem_using_with_considering_of_multiple_letters(marked_data, middle_data)    
+    features.mystem_using_with_considering_of_multiple_letters(middle_data, middle_data)
+
+    # убирает повторяющиеся буквы, фича, не насадка
+    print 'Удаляем подряд идущие повторяющиеся буквы...'  
+    features.considering_of_multiple_letters(middle_data)    
         
     # удаление предлогов
     #print 'Убираем предлоги...'
@@ -99,35 +103,35 @@ def main():
     #print 'Убираем местоимения...'
     #features.without_pronouns(middle_data)         
     
-    # убирает повторяющиеся буквы, фича, не насадка
-    print 'Удаляем подряд идущие повторяющиеся буквы...' 
-    features.considering_of_multiple_letters(middle_data)
-    
     # частица не
     print 'Присоединяем частицу не к словам не состояниям...'
     features.with_not(middle_data)    
     
     # убираем термы на английском языке
-    #print 'Убираем термы на английском языке...'
-    #features.without_foreign_words(middle_data)    
+    print 'Убираем термы на английском языке...'
+    features.without_foreign_words(middle_data)    
+    
+    # 1, 2, ..., n-граммы
+    #print '1, 2, 3, .. n граммы...'
+    #features.more_than_n_gram_feature(2, middle_data)
     
     # n-граммы
-   # print 'N граммы...'
-   # features.more_than_n_gram_feature(2, middle_data)
+    #print 'n граммы...'
+    #features.n_gram_feature(3, middle_data) 
     
     print 'Экстрактор работает...'    
     my_extractor = extractor.extractor(middle_data, extractor_data)
     if my_extractor.extract() == False:
         raise Exception('Error in extractor!')
-        
+
     print 'Запускаем машинное обучение...' 
-    #executeMlAndPrintAccurancy(naive_bayes_gaussian_count.NaiveBayesGaussian(extractor_data))
+    executeMlAndPrintAccurancy(naive_bayes_gaussian_count.NaiveBayesGaussian(extractor_data))
     executeMlAndPrintAccurancy(naive_bayes_multinomial_count.NaiveBayesMultinomial(extractor_data))
     #executeMlAndPrintAccurancy(svm_1_0.SVM(extractor_data))
     #executeMlAndPrintAccurancy(svm_tf_idf.SVM(extractor_data))
     #executeMlAndPrintAccurancy(logistic_regression_count.LG(extractor_data))
     
-    delete_folder_with_files(middle_data)    
+    #delete_folder_with_files(middle_data)    
     
 if __name__ == '__main__':
     main()
